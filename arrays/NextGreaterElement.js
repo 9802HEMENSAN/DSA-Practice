68acc48c6053e77dc424c5d5c624958a5dccb720
hemensan mahilange <hmahilange9802@gmail.com>
2023-01-25 00:36:03 +0530

    Next Greater Element

--- arrays/NextGreaterElement.js
+++ arrays/NextGreaterElement.js
@@ -33,3 +33,49 @@
 // Hint
 
 // In the sample test case, the array contains 4 elements, and for the first element, the next greater element will be 3, similarly for the next element, the next greater element will be 4, and for the third element as well, the next greater element will be 4. Since, the last element has no elements ahead of it, the next greater element will be -1.
+
+function  NearestGreater(N,arr){
+    let s=[], ans=[];
+for(let i=N-1; i>=0; i--){
+   while(s.length>0 && s[s.length-1]<=arr[i]){
+       s.pop();
+   }
+   if(s.length==0 || s==""){
+       ans.push(-1);
+   }else{
+       ans.push(s[s.length-1]);
+   }
+   s.push(arr[i]);
+}
+console.log(ans.reverse().join(" "));
+}
+
+
+function runProgram(input){
+  input=input.split("\n");
+  let tc=+input[0];
+  let line=1;
+for(let i=0; i<tc; i++){
+   let N=+input[line];
+   line++;
+   let arr=input[line].split(" ").map(Number);
+   line++;
+   NearestGreater(N,arr);
+ }
+}
+if (process.env.USERNAME === "") {
+   runProgram(``);
+} else {
+   process.stdin.resume();
+   process.stdin.setEncoding("ascii");
+   let read = "";
+   process.stdin.on("data", function (input) {
+       read += input; });
+   process.stdin.on("end", function () {
+       read = read.replace(/\n$/, "");
+       read = read.replace(/\n$/, "");
+       runProgram(read);});
+   process.on("SIGINT", function () {
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+       process.exit(0); }); }
